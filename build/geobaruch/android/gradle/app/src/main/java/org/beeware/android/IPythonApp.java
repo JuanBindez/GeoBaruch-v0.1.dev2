e7ea7ba0939532527828723cd0cc3dd68d1320da
JuanBindez <juanbindez780@gmail.com>
2023-05-14 17:52:13 -0300

    update v0.1-dev1 -> v0.1-dev2

---
+++ build/geobaruch/android/gradle/app/src/main/java/org/beeware/android/IPythonApp.java
@@ -0,0 +1,16 @@
+package org.beeware.android;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public interface IPythonApp {
+    void onCreate();
+    void onResume();
+    void onStart();
+    void onActivityResult(int requestCode, int resultCode, Intent data);
+    void onConfigurationChanged(Configuration newConfig);
+    boolean onOptionsItemSelected(MenuItem menuitem);
+    boolean onPrepareOptionsMenu(Menu menu);
+}
